0f32cb9eb95b4f9b8f8211b25c61933e6df8a3cc
さとやｎ <satoyanxp@gmail.com>
2025-08-17 18:36:49 +0900

    feat: User-made changes
    - Applied user-made changes to `google_places_details_api_datasource.dart`.

--- lib/features/detail/data/datasources/google_places_details_api_datasource.dart
+++ lib/features/detail/data/datasources/google_places_details_api_datasource.dart
@@ -5,11 +5,15 @@ import 'package:sweat_and_beers/core/utils/logger.dart';
 class GooglePlacesDetailsApiDataSource {
   final GoogleMapsPlaces _places;
 
-  GooglePlacesDetailsApiDataSource() : _places = GoogleMapsPlaces(apiKey: dotenv.env['GOOGLE_PLACES_API_KEY']);
+  GooglePlacesDetailsApiDataSource()
+    : _places = GoogleMapsPlaces(apiKey: dotenv.env['GOOGLE_PLACES_API_KEY']);
 
   Future<PlaceDetails> getPlaceDetails(String placeId) async {
     try {
-      final response = await _places.getDetailsByPlaceId(placeId);
+      final response = await _places.getDetailsByPlaceId(
+        placeId,
+        language: 'ja',
+      );
       if (response.status == 'OK') {
         return response.result;
       } else {
@@ -17,10 +21,16 @@ class GooglePlacesDetailsApiDataSource {
           'Google Places Details API Error: ${response.errorMessage}',
           error: response.status,
         );
-        throw Exception(response.errorMessage ?? 'Failed to load place details');
+        throw Exception(
+          response.errorMessage ?? 'Failed to load place details',
+        );
       }
     } catch (e, s) {
-      logger.e('Error in GooglePlacesDetailsApiDataSource', error: e, stackTrace: s);
+      logger.e(
+        'Error in GooglePlacesDetailsApiDataSource',
+        error: e,
+        stackTrace: s,
+      );
       rethrow;
     }
   }
